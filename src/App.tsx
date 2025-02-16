771a8c511a1841da7edac58c91f675ee5afb8346
hypertensiune <dariuscirneala05@gmail.com>
2025-02-16 23:08:48 +0200

    Separated AppMenu component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
-import './App.scss'
 
 import { enableMapSet } from 'immer'
+
+import { ProviderContext } from './context/ProviderContext'
+
+import AppMenu from './AppMenu.tsx'
 
 import ChooseTemplate from './pages/ChooseTemplate'
 import Home from './pages/Home'
 import Build from './pages/Build'
 import Me from './pages/Me'
-import Login from './pages/Login'
+import Signin from './pages/Signin'
 
-function Menu({children}: any) {
-
-  const [open, setOpen] = useState(false);
-
-  return (
-    <div className='menu'>
-      <div id='menutoggle' onClick={() => setOpen(!open)}>
-        <i className="fa-solid fa-bars"></i>
-      </div>
-      <div id='menu' className={open ? 'open' : ''}>
-        {children}
-      </div>
-    </div>
-  )
-}
+import './App.scss'
 
 function App() {
   enableMapSet();
 
   const [darkmode, setDarkmode] = useState(false);
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -48,35 +36,17 @@ function App() {
   }, [darkmode]);
 
   return (
-    <>
-      <Menu>
-        <div onClick={() => navigate('/me')}>
-          <span>My account</span>
-          <i className="fa-regular fa-user"></i>
-        </div>
-        <div onClick={() => navigate('/')}>
-          <span>Home</span>
-          <i className="fa-solid fa-house"></i>
-        </div>
-        <div onClick={() => setDarkmode(!darkmode)}>
-          <span>Theme</span>
-          <i className={`fa-solid ${darkmode ? 'fa-moon' : 'fa-sun'}`}></i>
-        </div>
-        <span className='delimiter'/>
-        <div onClick={() => navigate('/signin')}>
-          <span>Sign in</span>
-          <i className="fa-solid fa-arrow-right-to-bracket"></i>
-        </div>
-      </Menu>
+    <ProviderContext>
+      <AppMenu darkmode={darkmode} setDarkmode={setDarkmode}/>
       <Routes>
         <Route path='/' element={<Home darkmode={darkmode} />} />
         <Route path='/build' element={<Build />} />
         <Route path='/templates' element={<ChooseTemplate darkmode={darkmode} />} />
         <Route path='/me' element={<Me/>}/>
-        <Route path='/signin' element={<Login/>} />
+        <Route path='/signin' element={<Signin/>} />
         <Route path='*' element={<h1>404 Page not found!</h1>} />
       </Routes>
-    </>
+    </ProviderContext>
   )
 }
 
---
+++ src/AppMenu.tsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "./context/ProviderContext";
+
+export default function AppMenu({darkmode, setDarkmode}: any) {
+
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const services = useContext(AppContext);
+  const isAuthenticated = services.auth.isAuthenticated();
+
+  return (
+    <div className='menu'>
+      <div id='menutoggle' onClick={() => setOpen(!open)}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <div id='menu' className={open ? 'open' : ''}>
+        <div onClick={() => navigate('/me')}>
+          <span>My account</span>
+          <i className="fa-regular fa-user"></i>
+        </div>
+        <div onClick={() => navigate('/')}>
+          <span>Home</span>
+          <i className="fa-solid fa-house"></i>
+        </div>
+        <div onClick={() => setDarkmode(!darkmode)}>
+          <span>Theme</span>
+          <i className={`fa-solid ${darkmode ? 'fa-moon' : 'fa-sun'}`}></i>
+        </div>
+        <span className='delimiter'/>
+        <div onClick={() => {
+          if(isAuthenticated) {
+            services.auth.signOut();
+            navigate('/');
+          } else {
+            navigate('/signin');
+          }
+        }}>
+          <span>{isAuthenticated ? 'Sign Out' : 'Sign In'}</span>
+          <i className={`fa-solid ${isAuthenticated ? 'fa-arrow-right-from-bracket' : 'fa-arrow-right-to-bracket'}`}></i>
+        </div>
+      </div>
+    </div>
+  )
+}
