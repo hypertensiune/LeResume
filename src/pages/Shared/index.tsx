c15d79a99ac76c93487106a4561e26457316d2c1
hypertensiune <dariuscirneala05@gmail.com>
2025-02-20 18:18:04 +0200

    Fixed shared resume viewing

--- src/pages/Shared/index.tsx
+++ src/pages/Shared/index.tsx
@@ -1,14 +1,16 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 
-import './shared.scss';
-import { useContext, useEffect, useState } from "react";
 import { AppContext } from "@context/ProviderContext";
+
 import Resume from "@pages/Build/components/Resume";
+
+import './shared.scss';
 
 export default function Shared() {
 
   const { user, rid } = useParams();
-  const [searchParams] = useSearchParams();
+  const [_, setSearchParams] = useSearchParams();
   
   const services = useContext(AppContext);
 
@@ -17,7 +19,10 @@ export default function Shared() {
   useEffect(() => {
     services.db.getSharedResume(user!, rid!).then(res => {
       setResume(res);
-      searchParams.append('template', '1');
+      setSearchParams(prev => { 
+        prev.set('template', `${res?.template}`);
+        return prev;
+      });
     });
   }, []);
 
